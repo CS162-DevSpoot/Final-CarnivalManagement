d872a1b0da21601d09de537cbad4af4460aab7bb
DeveloperSpoot <52226518+DeveloperSpoot@users.noreply.github.com>
2024-06-05 15:24:19 -0700

    Add project files.

---
+++ Experience.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CS162_Final_CarnivalManagement
+{
+    public class Experience
+    {
+        public string Name { get; set; }
+        public int Capcitity { get; set; }
+        public decimal TicketPrice { get; set; }
+
+    }
+}
